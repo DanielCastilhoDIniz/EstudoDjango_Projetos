36e0a4bc61b2e1635ed81b06b366fa768c0e5194
Daniel Castilho <danielcastilhodiniz@gmail.com>
2023-08-06 02:33:47 -0300

    iniciando nova view

--- recipes/views.py
+++ recipes/views.py
@@ -5,4 +5,6 @@ def home(request):
     return render(request, 'recipes/pages/home.html')
 
 
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={'name': 'Daniel'})
 # Create your views here.
