481e0d8a26cf1df601867b8ab70537496319a0ea
Daniel Castilho <danielcastilhodiniz@gmail.com>
2023-11-03 01:35:48 -0300

    ajustes ccs

--- Projeto/settings.py
+++ Projeto/settings.py
@@ -57,6 +57,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             BASE_DIR /'base_templates/',
+            BASE_DIR /'utils/',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
